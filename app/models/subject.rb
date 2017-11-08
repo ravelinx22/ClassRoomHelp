dd2dc0fc674ff47a5dd8f0913d8a0022b3c4172b
William Ravelo <drummerwilliam@gmail.com>
2017-11-08 14:18:48 -0500

    fixed topic path from category view

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,10 +1,3 @@
-<<<<<<< HEAD
-class Subject
-  include Mongoid::Document
-
-  field :name, type: String
-end
-=======
 class Subject
   include Mongoid::Document
 
@@ -12,4 +5,3 @@ class Subject
 
   has_and_belongs_to_many :topics
 end
->>>>>>> 15c44b7a61191cb3966860e2947e8989559f592e
