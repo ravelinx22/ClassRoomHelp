bfef143e3e7c1cc55437fabe3449c41b135d9888
William Ravelo <drummerwilliam@gmail.com>
2017-11-07 22:26:05 -0500

    improved seeds.rb and added topics to subject with its view

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,7 +1,6 @@
-class Subject
+class Topic
   include Mongoid::Document
-
   field :name, type: String
 
-  has_and_belongs_to_many :topics
+  has_and_belongs_to_many :subjects 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,34 @@
 User.delete_all
+Topic.delete_all
 Subject.delete_all
 
 User.new(email: "drummerwilliam@gmail.com", password: "123456",password_confirmation: "123456").save!
 
-Subject.new(name: "Sistrans").save!
-Subject.new(name: "Probabilidad").save!
+topic1 = Topic.new(name: "Algebra relacional")
+topic1.save!
+topic2 = Topic.new(name: "Sentencias SQL")
+topic2.save!
+topic3 = Topic.new(name: "Tablas")
+topic3.save!
+topic4 = Topic.new(name: "Media, Mediana, Moda")
+topic4.save!
+topic5 = Topic.new(name: "Teorema Limite Central")
+topic5.save!
+topic6 = Topic.new(name: "Distribuciones")
+topic6.save!
+
+subject1 = Subject.new(name: "Sistrans")
+subject1.topics << topic1
+subject1.topics << topic2
+subject1.topics << topic3
+subject1.save!
+
+subject2 = Subject.new(name: "Probabilidad")
+subject2.topics << topic4
+subject2.topics << topic5
+subject2.topics << topic6
+subject2.save!
+
 Subject.new(name: "Diferencial").save!
 Subject.new(name: "Integral").save!
 Subject.new(name: "Lineal").save!
